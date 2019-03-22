7e1c749dfb13bf0dca925c83f237dc7397a7681e
brittanyabrahams2 <43973188+brittanyabrahams2@users.noreply.github.com>
2019-03-22 16:08:38 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello Britt!");
+
+        int myFirstNumber = 50 + 1;
+        int mySecondNumber = 100;
+        int myThirdNumber = 2;
+        int total = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(total);
+
+    }
+}
